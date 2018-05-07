c6c5febca0128b00d3ee63e87c93a2c1ce99b7d1
Tony Williams <honestpuck@gmail.com>
2018-05-07 11:55:33 +1000

    tools_convert early

--- tools_convert.py
+++ tools_convert.py
@@ -1,20 +1,42 @@
-info
+#
+# tools_convert.py
+# some routines to take JSS data as strings and convert
+# to python data types.
+#
+# v0.1
+# *very* early version, mnore a proof of concept than anything real.
+#
 
-import time
-
-seen = time.strptime(info_A['last'], '%Y-%m-%d %H:%M:%S')
+from time import strptime
+from datetime import datetime
 
 BOOL = 0
 DATE = 1
-INT  = 2
+INTN = 2
 TIME = 3
 
-def info_convert
 
-convert = [
-    ['id', 'I'],
-    ['initial', 'D'],
-    ['last', 'T'],
-    ['managed', 'B']
+def convert(dat):
+    return {
+        BOOL: lambda x: x == 'true',
+        INTN: lambda x: int(x),
+        DATE: lambda x: datetime.strptime(x, '%Y-%m-%d'),
+        TIME: lambda x: strptime(x, '%Y-%m-%d %H:%M:%S'),
+    }[dat[1]](dat[0])
+
+
+_info_convert_keys = [
+    ['id', BOOL],
+    ['initial', DATE],
+    ['last', TIME],
+    ['managed', BOOL]
 ]
+
+
+def info_convert(info):
+    for cc in _info_convert_keys:
+        info[cc[0]] = convert(cc)
+    return info
+
+
 
